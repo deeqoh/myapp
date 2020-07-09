ebe9cc3c83edcee7a6f87d64c09738a2fcb4188c
ismailabello <ismailabello1@gmail.com>
2020-07-09 15:11:19 +0100

    Updates

---
+++ precache-manifest.3618364639eee07a1d20995a31810b2c.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "917ecaa37585e97f027a771efba3c0d5",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "9699dc9249e43690b017",
+    "url": "/myapp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "79196a9462aa836916fe",
+    "url": "/myapp/static/css/main.ba478e25.chunk.css"
+  },
+  {
+    "revision": "9699dc9249e43690b017",
+    "url": "/myapp/static/js/2.3e89538c.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/myapp/static/js/2.3e89538c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79196a9462aa836916fe",
+    "url": "/myapp/static/js/main.37f35b99.chunk.js"
+  },
+  {
+    "revision": "2413d325ab07ed7ad194",
+    "url": "/myapp/static/js/runtime-main.0abbbebf.js"
+  },
+  {
+    "revision": "847d9e7be1812448afe0e77cd03cfc86",
+    "url": "/myapp/static/media/Cake.847d9e7b.jpg"
+  },
+  {
+    "revision": "7789e135c8a912c461a01b8719b1c901",
+    "url": "/myapp/static/media/bann.7789e135.jpg"
+  },
+  {
+    "revision": "c81e50a41a28e75e5007aa9b709fd6a9",
+    "url": "/myapp/static/media/banner.c81e50a4.jpg"
+  },
+  {
+    "revision": "3d68a8a0da8b640920e8a4b8271c99e2",
+    "url": "/myapp/static/media/banner1.3d68a8a0.jpg"
+  },
+  {
+    "revision": "9213ff89d0840a69f636fe5fc0df5667",
+    "url": "/myapp/static/media/cake1.9213ff89.jpg"
+  },
+  {
+    "revision": "f8e9d41d6492117cc095d41e2667f185",
+    "url": "/myapp/static/media/cake10.f8e9d41d.jpg"
+  },
+  {
+    "revision": "cb37f46fca6fa7e5621f570c54f346cd",
+    "url": "/myapp/static/media/cake11.cb37f46f.jpg"
+  },
+  {
+    "revision": "8333a90e6a06a7c11a5efb973aa6c3d6",
+    "url": "/myapp/static/media/cake12.8333a90e.jpg"
+  },
+  {
+    "revision": "e28bf6053e3f49380fa0d4513f943490",
+    "url": "/myapp/static/media/cake13.e28bf605.jpg"
+  },
+  {
+    "revision": "444f29a6532d59b375fef5fa7e66020d",
+    "url": "/myapp/static/media/cake14.444f29a6.jpg"
+  },
+  {
+    "revision": "d58a438e4fe1e1e7c3f268186a184589",
+    "url": "/myapp/static/media/cake15.d58a438e.jpg"
+  },
+  {
+    "revision": "5fb66367de0ce4db4dd38283c3d36f49",
+    "url": "/myapp/static/media/cake16.5fb66367.jpg"
+  },
+  {
+    "revision": "6cee2ebd394f3851c31114832377405a",
+    "url": "/myapp/static/media/cake17.6cee2ebd.jpg"
+  },
+  {
+    "revision": "bd379011d797b7e5213fbc2d82bb07f5",
+    "url": "/myapp/static/media/cake18.bd379011.jpg"
+  },
+  {
+    "revision": "8249673d2622a629cafe45019760a56e",
+    "url": "/myapp/static/media/cake19.8249673d.jpg"
+  },
+  {
+    "revision": "25a6c76e5411b559782c0b4afef71782",
+    "url": "/myapp/static/media/cake2.25a6c76e.jpg"
+  },
+  {
+    "revision": "8601cf1f07eaae13d2d2494ea5d12ad6",
+    "url": "/myapp/static/media/cake20.8601cf1f.jpg"
+  },
+  {
+    "revision": "6f88ff84b44f88aac58532ab5bef567b",
+    "url": "/myapp/static/media/cake21.6f88ff84.jpg"
+  },
+  {
+    "revision": "945ceb06348c09dfb3cb71ea3a60f5be",
+    "url": "/myapp/static/media/cake22.945ceb06.jpg"
+  },
+  {
+    "revision": "d10079353d40d4459349e8e2019243ac",
+    "url": "/myapp/static/media/cake23.d1007935.jpg"
+  },
+  {
+    "revision": "a9dcb366169231899c90491ea8471fcb",
+    "url": "/myapp/static/media/cake24.a9dcb366.jpg"
+  },
+  {
+    "revision": "f73d5e7be6e1922009ee82eaae833b22",
+    "url": "/myapp/static/media/cake25.f73d5e7b.jpg"
+  },
+  {
+    "revision": "7460574897542206e7b8ce1ba0fdc629",
+    "url": "/myapp/static/media/cake26.74605748.jpg"
+  },
+  {
+    "revision": "0b018bf69fa18bdc793b7ecbfe7ad164",
+    "url": "/myapp/static/media/cake27.0b018bf6.jpg"
+  },
+  {
+    "revision": "3ad71337b461abbcd5ef110940a96889",
+    "url": "/myapp/static/media/cake3.3ad71337.jpg"
+  },
+  {
+    "revision": "177241af2aecc03639745c74ad295227",
+    "url": "/myapp/static/media/cake4.177241af.jpg"
+  },
+  {
+    "revision": "5b9769eac6123fe39a52158c4a3ccebb",
+    "url": "/myapp/static/media/cake5.5b9769ea.jpg"
+  },
+  {
+    "revision": "7ed2b5bb0cb5d2d4bf9130ce17bbc37d",
+    "url": "/myapp/static/media/cake6.7ed2b5bb.jpg"
+  },
+  {
+    "revision": "b84f863e27d1d14a7b696b74c393f938",
+    "url": "/myapp/static/media/cake7.b84f863e.jpg"
+  },
+  {
+    "revision": "0db65326aa4132ecd43680526d181d74",
+    "url": "/myapp/static/media/cake8.0db65326.jpg"
+  },
+  {
+    "revision": "3c865aaebf0e96530bb5f4d5bd3fcc10",
+    "url": "/myapp/static/media/cake9.3c865aae.jpg"
+  }
+]);
